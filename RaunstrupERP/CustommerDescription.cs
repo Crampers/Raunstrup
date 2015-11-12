f1773738e332b94d769b13118de1b8908678e71a
AndersSejr <mail@anderssejr.dk>
2015-11-12 10:46:08 +0100

    custommerDesciption

---
+++ RaunstrupERP/CustommerDescription.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaunstrupERP
+{
+    public class CustommerDescription
+    {
+        public int customerID;
+        public string FirstName;
+        public string SirName;
+        public string Adress;
+        public int PostNr;
+        public string By;
+        public int Tlf;
+
+        public CustommerDescription (int ID, string fn, string sn, string adress, int zip, string city, int phone)
+        {
+            customerID = ID;
+            FirstName = fn;
+            SirName = sn;
+            Adress = adress;
+            PostNr = zip;
+            By = city;
+            Tlf = phone;     
+
+        }
+    }
+}
