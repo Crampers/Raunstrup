bd5cfaa073c0b1fe1962825dc43ccfe06501baeb
tranquiliza <daniel.olsen.server@gmail.com>
2015-11-16 11:15:24 +0100

    ItemDescription Commit

---
+++ RaunstrupERP/ItemCatalog.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaunstrupERP
+{
+    class ItemCatalog
+    {
+        private List<ItemDescription> items;
+        public ItemCatalog()
+        {
+            items = new List<ItemDescription>();
+            items.Add(new ItemDescription("Test Vare  ", 1, 10, 8));
+            items.Add(new ItemDescription("Test Vare 2", 2, 50, 14));
+            items.Add(new ItemDescription("Test Vare 3", 3, 100, 25));
+        }
+        /*Getters*/
+        public ItemDescription getItem(int id)
+        {
+            ItemDescription item = items.Find(ItemDescription => ItemDescription.getID() == id);
+            return item;
+        }
+        /*Finds the next available ID above current existing IDs
+        Like a baws!*/
+        public int getNewId()
+        {
+            int newID = 0;
+            foreach (ItemDescription item in items)
+            {
+                if (item.getID() > newID)
+                {
+                    newID = item.getID();
+                }
+            }
+            newID = newID + 1;
+            return newID;
+        }
+
+        /*Setters*/
+        public void addItem(ItemDescription item)
+        {
+            items.Add(item);
+        }
+        public void updateItemDesc(int id, String newDesc)
+        {
+            foreach (ItemDescription item in items)
+            {
+                if (item.getID() == id)
+                {
+                    item.updateDesc(newDesc);
+                }
+            }
+        }
+        public void updateItemSalesPrice(int id, Double newPrice)
+        {
+            foreach (ItemDescription item in items)
+            {
+                if (item.getID() == id)
+                {
+                    item.updateSalesPrice(newPrice);
+                }
+            }
+        }
+        public void updateItemShopsPrice(int id, Double newPrice)
+        {
+            foreach (ItemDescription item in items)
+            {
+                if (item.getID() == id)
+                {
+                    item.updateShopsPrice(newPrice);
+                }
+            }
+        }
+        
+        /*TESTERS*/
+        public void printItem(int id)
+        {
+            ItemDescription item = items.Find(ItemDescription => ItemDescription.getID() == id);
+            item.printItem();
+        }
+        public void printAllItems()
+        {
+            foreach (ItemDescription item in items)
+            {
+                item.printItem();
+            }
+        }
+    }
+}
---
+++ RaunstrupERP/ItemDescription.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaunstrupERP
+{
+    class ItemDescription
+    {
+        /*Members*/
+        private String description;
+        private int itemID;
+        private double salesPrice;
+        private double shopsPrice;
+
+        public ItemDescription(String desc, int id, double salesPrice, double shopsPrice)
+        {
+            this.description = desc;
+            this.itemID = id;
+            this.salesPrice = salesPrice;
+            this.shopsPrice = shopsPrice;
+        }
+        /*Getters*/
+        public int getID()
+        {
+            return this.itemID;
+        }
+        public String getDesc()
+        {
+            return this.description;
+        }
+        public double getSalesPrice()
+        {
+            return this.salesPrice;
+        }
+        public double getShopsPrice()
+        {
+            return this.shopsPrice;
+        }
+
+        /*Setters*/
+        public void updateDesc(String newDesc)
+        {
+            this.description = newDesc;
+        }
+        public void updateItemID(int newID)
+        {
+            this.itemID = newID;
+        }
+        public void updateSalesPrice(double newPrice)
+        {
+            this.salesPrice = newPrice;
+        }
+        public void updateShopsPrice(double newPrice)
+        {
+            this.shopsPrice = newPrice;
+        }
+
+        /*TESTERS*/
+        public void printItem()
+        {
+            Console.WriteLine(this.description 
+                + ", ID: " + this.itemID 
+                + ", salgspris: " + this.salesPrice + "kr. " 
+                + "indkøbspris: " + this.shopsPrice + "kr. ");
+        }
+    }
+}
