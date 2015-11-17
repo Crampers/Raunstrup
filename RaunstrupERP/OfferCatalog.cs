3cbd22d7524c535832cfe10a95efd7f51d2caeeb
tranquiliza <daniel.olsen.server@gmail.com>
2015-11-17 10:55:05 +0100

    Tilbud plus TilbudCatalog

---
+++ RaunstrupERP/OfferCatalog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaunstrupERP
+{
+    class OfferCatalog
+    {
+        List<OfferDescription> Offers;
+        public OfferCatalog()
+        {
+            Offers = new List<OfferDescription>();
+        }
+        /*GETTERS*/
+        public OfferDescription getOfferID(int id)
+        {
+            OfferDescription returnMe = null;
+            foreach (OfferDescription item in Offers)
+            {
+                if (item.getID() == id)
+                {
+                    returnMe = item;
+                }
+            }
+            return returnMe;
+        }
+
+        /*SETTERS*/
+        public void addOffer(OfferDescription offer)
+        {
+            Offers.Add(offer);
+        }
+        
+        /*TESTERS*/
+
+    }
+}
---
+++ RaunstrupERP/OfferDescription.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaunstrupERP
+{
+    class OfferDescription
+    {
+        int offerID;
+        TaskCatalog workTasks;
+        CustomerDescription buyer;
+        EmployeeDescription salesman;
+        public OfferDescription(int id, TaskCatalog tc)
+        {
+            offerID = id;
+            workTasks = tc;
+        }
+        /*Getters*/
+        public int getID()
+        {
+            return this.offerID;
+        }
+        public TaskCatalog getWorkTasks()
+        {
+            return this.workTasks;
+        }
+        public CustomerDescription getBuyer()
+        {
+            return this.buyer;
+        }
+        public EmployeeDescription getSalesMan()
+        {
+            return this.salesman;
+        }
+
+        /*Setters*/
+        public void setID(int id)
+        {
+            this.offerID = id;
+        }
+        public void setWorkTasks(TaskCatalog tc)
+        {
+            this.workTasks = tc;
+        }
+        public void setBuyer(CustomerDescription buyer)
+        {
+            this.buyer = buyer;
+        }
+        public void setSalesMan(EmployeeDescription salesMan)
+        {
+            this.salesman = salesMan;
+        }
+
+        /*Testers*/
+    }
+}
