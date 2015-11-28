b688bd84d60e125487e740d4de73248b9b0f16bf
tranquiliza <daniel.olsen.server@gmail.com>
2015-11-29 00:55:48 +0100

    ccccchanges :D

--- RaunstrupERP/Form_Employee_1.Designer.cs
+++ RaunstrupERP/Form_Employee_1.Designer.cs
@@ -45,13 +45,14 @@
             this.textBox_City = new System.Windows.Forms.TextBox();
             this.textBox_Phone = new System.Windows.Forms.TextBox();
             this.textBox_Salary = new System.Windows.Forms.TextBox();
-            this.textBox_Special = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox_EmployeesSelect = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label_Id
             // 
             this.label_Id.AutoSize = true;
-            this.label_Id.Location = new System.Drawing.Point(25, 17);
+            this.label_Id.Location = new System.Drawing.Point(285, 12);
             this.label_Id.Name = "label_Id";
             this.label_Id.Size = new System.Drawing.Size(16, 13);
             this.label_Id.TabIndex = 0;
@@ -60,7 +61,7 @@
             // label_FirstName
             // 
             this.label_FirstName.AutoSize = true;
-            this.label_FirstName.Location = new System.Drawing.Point(22, 43);
+            this.label_FirstName.Location = new System.Drawing.Point(282, 38);
             this.label_FirstName.Name = "label_FirstName";
             this.label_FirstName.Size = new System.Drawing.Size(46, 13);
             this.label_FirstName.TabIndex = 1;
@@ -69,7 +70,7 @@
             // label_SirName
             // 
             this.label_SirName.AutoSize = true;
-            this.label_SirName.Location = new System.Drawing.Point(22, 69);
+            this.label_SirName.Location = new System.Drawing.Point(282, 64);
             this.label_SirName.Name = "label_SirName";
             this.label_SirName.Size = new System.Drawing.Size(53, 13);
             this.label_SirName.TabIndex = 2;
@@ -78,7 +79,7 @@
             // label_Adress
             // 
             this.label_Adress.AutoSize = true;
-            this.label_Adress.Location = new System.Drawing.Point(22, 95);
+            this.label_Adress.Location = new System.Drawing.Point(282, 90);
             this.label_Adress.Name = "label_Adress";
             this.label_Adress.Size = new System.Drawing.Size(45, 13);
             this.label_Adress.TabIndex = 3;
@@ -87,7 +88,7 @@
             // label_PostalCode
             // 
             this.label_PostalCode.AutoSize = true;
-            this.label_PostalCode.Location = new System.Drawing.Point(22, 121);
+            this.label_PostalCode.Location = new System.Drawing.Point(282, 116);
             this.label_PostalCode.Name = "label_PostalCode";
             this.label_PostalCode.Size = new System.Drawing.Size(65, 13);
             this.label_PostalCode.TabIndex = 4;
@@ -96,7 +97,7 @@
             // label_City
             // 
             this.label_City.AutoSize = true;
-            this.label_City.Location = new System.Drawing.Point(22, 147);
+            this.label_City.Location = new System.Drawing.Point(282, 142);
             this.label_City.Name = "label_City";
             this.label_City.Size = new System.Drawing.Size(43, 13);
             this.label_City.TabIndex = 5;
@@ -105,7 +106,7 @@
             // label_Phone
             // 
             this.label_Phone.AutoSize = true;
-            this.label_Phone.Location = new System.Drawing.Point(22, 173);
+            this.label_Phone.Location = new System.Drawing.Point(282, 168);
             this.label_Phone.Name = "label_Phone";
             this.label_Phone.Size = new System.Drawing.Size(43, 13);
             this.label_Phone.TabIndex = 6;
@@ -114,7 +115,7 @@
             // label_Salary
             // 
             this.label_Salary.AutoSize = true;
-            this.label_Salary.Location = new System.Drawing.Point(22, 199);
+            this.label_Salary.Location = new System.Drawing.Point(282, 194);
             this.label_Salary.Name = "label_Salary";
             this.label_Salary.Size = new System.Drawing.Size(25, 13);
             this.label_Salary.TabIndex = 7;
@@ -123,7 +124,7 @@
             // label_Special
             // 
             this.label_Special.AutoSize = true;
-            this.label_Special.Location = new System.Drawing.Point(22, 225);
+            this.label_Special.Location = new System.Drawing.Point(282, 220);
             this.label_Special.Name = "label_Special";
             this.label_Special.Size = new System.Drawing.Size(48, 13);
             this.label_Special.TabIndex = 8;
@@ -131,73 +132,83 @@
             // 
             // textBox_Id
             // 
-            this.textBox_Id.Location = new System.Drawing.Point(93, 14);
+            this.textBox_Id.Location = new System.Drawing.Point(353, 9);
             this.textBox_Id.Name = "textBox_Id";
             this.textBox_Id.Size = new System.Drawing.Size(100, 20);
             this.textBox_Id.TabIndex = 9;
             // 
             // textBox_FirstName
             // 
-            this.textBox_FirstName.Location = new System.Drawing.Point(93, 40);
+            this.textBox_FirstName.Location = new System.Drawing.Point(353, 35);
             this.textBox_FirstName.Name = "textBox_FirstName";
             this.textBox_FirstName.Size = new System.Drawing.Size(100, 20);
             this.textBox_FirstName.TabIndex = 10;
             // 
             // textBox_SirName
             // 
-            this.textBox_SirName.Location = new System.Drawing.Point(93, 66);
+            this.textBox_SirName.Location = new System.Drawing.Point(353, 61);
             this.textBox_SirName.Name = "textBox_SirName";
             this.textBox_SirName.Size = new System.Drawing.Size(100, 20);
             this.textBox_SirName.TabIndex = 11;
             // 
             // textBox_Adress
             // 
-            this.textBox_Adress.Location = new System.Drawing.Point(93, 92);
+            this.textBox_Adress.Location = new System.Drawing.Point(353, 87);
             this.textBox_Adress.Name = "textBox_Adress";
             this.textBox_Adress.Size = new System.Drawing.Size(100, 20);
             this.textBox_Adress.TabIndex = 12;
             // 
             // textBox_PostalCode
             // 
-            this.textBox_PostalCode.Location = new System.Drawing.Point(93, 118);
+            this.textBox_PostalCode.Location = new System.Drawing.Point(353, 113);
             this.textBox_PostalCode.Name = "textBox_PostalCode";
             this.textBox_PostalCode.Size = new System.Drawing.Size(100, 20);
             this.textBox_PostalCode.TabIndex = 13;
             // 
             // textBox_City
             // 
-            this.textBox_City.Location = new System.Drawing.Point(93, 144);
+            this.textBox_City.Location = new System.Drawing.Point(353, 139);
             this.textBox_City.Name = "textBox_City";
             this.textBox_City.Size = new System.Drawing.Size(100, 20);
             this.textBox_City.TabIndex = 14;
             // 
             // textBox_Phone
             // 
-            this.textBox_Phone.Location = new System.Drawing.Point(93, 170);
+            this.textBox_Phone.Location = new System.Drawing.Point(353, 165);
             this.textBox_Phone.Name = "textBox_Phone";
             this.textBox_Phone.Size = new System.Drawing.Size(100, 20);
             this.textBox_Phone.TabIndex = 15;
             // 
             // textBox_Salary
             // 
-            this.textBox_Salary.Location = new System.Drawing.Point(93, 196);
+            this.textBox_Salary.Location = new System.Drawing.Point(353, 191);
             this.textBox_Salary.Name = "textBox_Salary";
             this.textBox_Salary.Size = new System.Drawing.Size(100, 20);
             this.textBox_Salary.TabIndex = 16;
             // 
-            // textBox_Special
+            // comboBox1
             // 
-            this.textBox_Special.Location = new System.Drawing.Point(93, 222);
-            this.textBox_Special.Name = "textBox_Special";
-            this.textBox_Special.Size = new System.Drawing.Size(100, 20);
-            this.textBox_Special.TabIndex = 17;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(353, 220);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // listBox_EmployeesSelect
+            // 
+            this.listBox_EmployeesSelect.FormattingEnabled = true;
+            this.listBox_EmployeesSelect.Location = new System.Drawing.Point(12, 12);
+            this.listBox_EmployeesSelect.Name = "listBox_EmployeesSelect";
+            this.listBox_EmployeesSelect.Size = new System.Drawing.Size(267, 264);
+            this.listBox_EmployeesSelect.TabIndex = 18;
             // 
             // Form_Employee_1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(742, 484);
-            this.Controls.Add(this.textBox_Special);
+            this.Controls.Add(this.listBox_EmployeesSelect);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox_Salary);
             this.Controls.Add(this.textBox_Phone);
             this.Controls.Add(this.textBox_City);
@@ -241,6 +252,7 @@
         private System.Windows.Forms.TextBox textBox_City;
         private System.Windows.Forms.TextBox textBox_Phone;
         private System.Windows.Forms.TextBox textBox_Salary;
-        private System.Windows.Forms.TextBox textBox_Special;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox_EmployeesSelect;
     }
 }
