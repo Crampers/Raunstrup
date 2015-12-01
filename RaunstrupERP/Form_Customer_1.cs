415cea4097e95a1407cff90b44d81a7e254cf165
tranquiliza <daniel.olsen.server@gmail.com>
2015-12-01 11:58:16 +0100

    First Implementation of the DB Controller. Incoming merge!

--- RaunstrupERP/Form_Customer_1.cs
+++ RaunstrupERP/Form_Customer_1.cs
@@ -12,9 +12,18 @@ namespace RaunstrupERP
 {
     public partial class Form_Customer_1 : Form
     {
+        ControllerCatalog cc = new ControllerCatalog();
         public Form_Customer_1()
         {
             InitializeComponent();
+        }
+
+        private void button_SearchCustomer_Click(object sender, EventArgs e)
+        {
+            int customerID = Convert.ToInt32(textBox_CustomerId.Text);
+            textBox_FirstName.Text = cc.FindCustomer(customerID).GetFirstName();
+            textBox_SirName.Text = cc.FindCustomer(customerID).GetSurName();
+            textBox_Adress.Text = cc.FindCustomer(customerID).GetAdresses()[1];
         }
     }
 }
--- RaunstrupERP/Program.cs
+++ RaunstrupERP/Program.cs
@@ -12,12 +12,11 @@ namespace RaunstrupERP
         static void Main(string[] args)
         {
             ControllerCatalog ccc = new ControllerCatalog();
-            Test_Data_Class TDC = new Test_Data_Class();
+            //Test_Data_Class TDC = new Test_Data_Class();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form_Main_1());
             Console.ReadKey();
-
             
             ///* THIS IS A TEST PROGRAM! COMMENT IT OUT WHEN WORKING ON NEXT STEPS! */
             //Console.WriteLine("Employees!");
